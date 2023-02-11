03b7cfe7fbd36c41d713ac1e55f8fd4bcf55ca8b
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-11 10:08:48 +0100

    code review

--- study_hub/BLOG/base/models.py
+++ study_hub/BLOG/base/models.py
@@ -36,7 +36,7 @@ class Message(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated', '-created']
+        ordering = ['updated', 'created']
 
     def __str__(self):
         return self.body[0:50]
