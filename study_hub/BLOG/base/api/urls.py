55b42bd6cb7c5b0b019888fe8b5f7a8328ee1c3f
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-12 01:19:12 +0100

    building api(serializers)

--- study_hub/BLOG/base/api/urls.py
+++ study_hub/BLOG/base/api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRoom),
 
 ]
--- study_hub/BLOG/base/api/views.py
+++ study_hub/BLOG/base/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -11,7 +13,15 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-
+@api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
